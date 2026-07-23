50eb7848781e82e13106970319bc337298e815b3
CML90 <caitlincebu@hotmail.com>
2026-07-23 15:31:16 +0800

    feat: complete exercise 09_sumAll

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function (numStart, numEnd) {
 
+    if(!Number.isInteger(numStart) || !Number.isInteger(numEnd)) return "ERROR";
+    if(numStart < 0 || numEnd < 0) return "ERROR";
+
+    if (numEnd < numStart) {
+        let temp = numStart;
+        numStart = numEnd;
+        numEnd = temp;
+    }
+
+    let sum = 0;
+    for (numStart; numStart <= numEnd; numStart++) {
+        sum += numStart;
+    }
+    return sum;
 };
 
 // Do not edit below this line
