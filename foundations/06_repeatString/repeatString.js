7deb5edf18112345cfa5f3563f7bb3ec2cab33a7
CML90 <caitlincebu@hotmail.com>
2026-07-23 11:36:13 +0800

    feat: complete exercise 06_repeatedStrings

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(str, num) {
 
+    if(num < 0){return "ERROR"}
+
+    let repeatedString = "";
+
+    for(let x = 0; x < num; x++){
+        repeatedString += str;
+    }
+
+    return repeatedString;
 };
 
 // Do not edit below this line
